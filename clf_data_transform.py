54f60e050b588004c3b7b65b5af042470369849e
Ivan Poplavskiy <didges73@gmail.com>
2023-06-05 15:25:25 +0300

    F1=0.98

--- clf_data_transform.py
+++ clf_data_transform.py
@@ -3,117 +3,77 @@ import pandas as pd
 import json
 import pyedflib
 import multiprocessing
-import pyeeg
 import os
+import pywt
+import pyeeg
+import scipy
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter("ignore", UserWarning)
 
 
 FREQUENCY = 256
-NUM_FEATURES_FOR_CHANEL = 31
-WINDOW_SIZE = 30
+NUM_FEATURES_FOR_CHANEL = 54
+WINDOW_SIZE = 5
+NON_SEIZURE_WINDOW = 8
 CHANELS = ['FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'FP2-F4', 'F4-C4', 'C4-P4',
            'P4-O2', 'FP2-F8', 'F8-T8', 'T8-P8', 'P8-O2', 'FZ-CZ', 'CZ-PZ', 'P7-T7', 'T7-FT9', 'FT9-FT10', 'FT10-T8']
 SEIZURE_JSON_PATH = 'seizures_time.json'
 
 
-def metrics(mat):
-    """
-    Функция принимает на вход 1d сигнал и возвращает tuple из 31 метрики
-    """
-    Kmax = 5
-    Tau = 4
-    DE = 10
-    M = 10
-    R = 0.3
-    Band = np.arange(1, 86)
-    Fs = 173
-    DFA = pyeeg.dfa(mat)
-    HFD = pyeeg.hfd(mat, Kmax)
-    SVD_Entropy = pyeeg.svd_entropy(mat, Tau, DE)
-    Fisher_Information = pyeeg.fisher_info(mat, Tau, DE)
-    PFD = pyeeg.pfd(mat)
+def metrics(signal):
+    features = np.array([])
+    coeffs = pywt.wavedec(signal, "coif3", level=7)
+    cA7, cD7, cD6, cD5, cD4, cD3, cD2, cD1 = coeffs
+    for i in range(signal.shape[0]):
+        for c_level in [cD1, cD2, cD3, cD4, cD5, cD6, cD7]:
+            features = np.append(features, np.sum(c_level[i] ** 2))
+            features = np.append(features, np.max(c_level[i]))
+            features = np.append(features, np.min(c_level[i]))
+            features = np.append(features, np.mean(c_level[i]))
+            features = np.append(features, np.std(c_level[i]))
+            features = np.append(features, scipy.stats.skew(c_level[i]))
+            features = np.append(features, scipy.stats.kurtosis(c_level[i]))
 
-    feature_pandas = pd.DataFrame(np.array(mat))
-    data_transform = feature_pandas.describe().iloc[1:, :]
+        Kmax = 5
+        Tau = 4
+        DE = 10
+        DFA = pyeeg.dfa(signal[i])
+        HFD = pyeeg.hfd(signal[i], Kmax)
+        SVD_Entropy = pyeeg.svd_entropy(signal[i], Tau, DE)
+        Fisher_Information = pyeeg.fisher_info(signal[i], Tau, DE)
+        PFD = pyeeg.pfd(signal[i])
 
-    # Коэффициент асимметрии
-    feature_pandas.loc['skew'] = feature_pandas.skew().tolist()
+        features = np.append(features, [DFA, HFD, SVD_Entropy, Fisher_Information, PFD])
 
-    # Среднее абсолютное отклонение
-    data_transform.loc['mad'] = feature_pandas.mad().tolist()
-
-    # Коэффициент эксцесса — мера остроты пика распределения случайной величины.
-    data_transform.loc['kurtosis'] = feature_pandas.kurtosis().tolist()
-
-    # добавление квантилей
-    for i in range(0, 100, 5):
-        if (i != 25) & (i != 50) & (i != 75):
-            str_col = f"{i}%"
-            int_col = float(i) / 100
-            data_transform.loc[str_col] = feature_pandas.quantile(int_col).tolist()
-        else:
-            continue
-
-    pd_f = data_transform.T.values[0].tolist()
-
-    return (DFA, HFD, SVD_Entropy, Fisher_Information, PFD) + tuple(pd_f)
-
-
-def end_extraction(response, name="dataset_shiftwindow_31metrics.csv"):
-    """
-    Завершающий callback для параллельного подсчета, записывает данные в csv файл
-    """
-    counter = 0
-    try:
-        for r in response:
-            for el in r:
-                counter += 1
-                data.loc[len(data.index)] = el
-        print(f"{counter} observations will be write")
-        data.to_csv(name)
-    except Exception as e:
-        print(f"EXCEPTION:{e}")
+    return features
 
 
 def select_wind(sample, end):
-    """
-    Функция выбирает окна, которые будут выбраны для обработки.
-    Всегда выбирается 5 случайных окон в которых нет таргета.
-    Для каждого таргета выбирается по 3 окна со случайным смещением начала.
-    Возвращает массив с началом выбранных окон, массив является ли окно таргетом, смещение
-    """
     seizure_data = json.load(open(SEIZURE_JSON_PATH))
     is_seizure = np.array([], dtype=int)
-    start_time_in_window = np.array([])
     if sample in seizure_data.keys():
         selected_windows = np.array([], dtype=int)
         seiz_windows = np.array(seizure_data[sample]['start_time'])
         seiz_windows_end = np.array(seizure_data[sample]['end_time'])
-        rand_added = 0
-        while rand_added < 5:
-            candidate = np.random.randint(0, high=end-WINDOW_SIZE*FREQUENCY)
+        for cand in np.random.randint(0, end-FREQUENCY*WINDOW_SIZE, NON_SEIZURE_WINDOW):
             fits = True
             for s in range(len(seiz_windows)):
-                if seiz_windows_end[s] > s > seiz_windows[s]:
+                if seiz_windows_end[s] > cand > seiz_windows[s]:
                     fits = False
             if fits:
-                selected_windows = np.append(selected_windows, candidate)
+                selected_windows = np.append(selected_windows, cand)
                 is_seizure = np.append(is_seizure, 0)
-                rand_added += 1
-                start_time_in_window = np.append(start_time_in_window, None)
 
-        shift_windows = np.random.randint(0, 9), np.random.randint(10, 19), np.random.randint(20, 25)
-        for shift_sample in seiz_windows:
-            for shift_wind in shift_windows:
-                selected_windows = np.append(selected_windows, (shift_sample-shift_wind)*256)
-                start_time_in_window = np.append(start_time_in_window, shift_wind)
-
-        is_seizure = np.append(is_seizure, [1 for _ in range(len(seiz_windows)*3)])
+        for seiz_start, seiz_end in zip(seiz_windows, seiz_windows_end):
+            selected_windows = np.append(selected_windows, [seiz_start + i*WINDOW_SIZE for i in
+                                                            range((seiz_end - seiz_start) // WINDOW_SIZE)])
+            is_seizure = np.append(is_seizure, [1 for _ in range((seiz_end - seiz_start) // WINDOW_SIZE)])
     else:
-        selected_windows = np.random.randint(0, high=end-WINDOW_SIZE*FREQUENCY, size=5, dtype=int)
-        is_seizure = np.append(is_seizure, [0 for _ in range(len(selected_windows))])
-        start_time_in_window = np.append(start_time_in_window, [None for _ in range(len(selected_windows))])
+        selected_windows = np.random.randint(0, end-FREQUENCY*WINDOW_SIZE, NON_SEIZURE_WINDOW)
+        is_seizure = np.append(is_seizure, [0 for _ in range(NON_SEIZURE_WINDOW)])
 
-    return selected_windows, is_seizure, start_time_in_window
+    return selected_windows, is_seizure
 
 
 def read_sig(sample_path):
@@ -124,8 +84,15 @@ def read_sig(sample_path):
         for i, label in enumerate(labels):
             if label in set(CHANELS):
                 sample_channels[label] = data_sample.readSignal(i)
+        len_ch = len(sample_channels[CHANELS[0]])
 
-    return sample_channels, len(sample_channels[CHANELS[0]])
+        data = np.empty((22, len_ch))
+        for i, ch in enumerate(CHANELS):
+            data[i] = np.array(sample_channels[ch])
+
+        data_sample.close()
+        return data, len_ch
+    return 0, 0
 
 
 def features_extraction(sample):
@@ -133,40 +100,34 @@ def features_extraction(sample):
         np.random.seed(int(os.getpid()*14))
         sample_channels, len_sig = read_sig(f"{dataset_path}/{sample}")
         if len(sample_channels):
-            selected_windows, is_seizure, shift = select_wind(sample, len_sig)
-            result = []
-            for sel, is_seiz, sh in zip(selected_windows, is_seizure, shift):
+            selected_windows, is_seizure = select_wind(sample, len_sig)
+            data_features = np.array([[f'f{i}'] for i in range(NUM_FEATURES_FOR_CHANEL * 22)]).reshape(-1)
+            data = pd.DataFrame(data=None, columns=['name'] + data_features.tolist())
+            data['target'] = None
+            for sel, is_seiz in zip(selected_windows, is_seizure):
                 indices = np.arange(sel, sel + WINDOW_SIZE*FREQUENCY, dtype=int)
-                features = np.array([])
                 sample_features = {'name': f"{sample}_{sel}"}
-                for ch in CHANELS:
-                    features = np.append(features, metrics(sample_channels[ch][indices]), axis=0)
-                for i, feature in enumerate(features.tolist()):
+
+                features = metrics(sample_channels[:, indices])
+                for i, feature in enumerate(features):
                     sample_features[f'f{i}'] = feature
 
                 sample_features['target'] = is_seiz
-                sample_features['shift'] = sh
 
                 print(sample_features['name'])
-                result.append(sample_features)
-            return result
-        else:
-            return []
+                data.loc[len(data.index)] = sample_features
+
+            data.to_csv(f'data/{sample.split("/")[1]}.csv')
+
     except Exception as e:
         print(f"EXCEPTION: {e}")
 
 
 if __name__ == '__main__':
-    dataset_path = "../EEG/physionet.org/files/chbmit/1.0.0/"
+    dataset_path = "/home/didges/ds/eeg_project/EEG/physionet.org/files/chbmit/1.0.0"
     all_samples = open(f"{dataset_path}/RECORDS", "r").read().split("\n")[:-1]
-    data_features = np.array([[f'f{i}'] for i in range(NUM_FEATURES_FOR_CHANEL*22)]).reshape(-1)
-    data = pd.DataFrame(data=None, columns=['name'] + data_features.tolist())
-    data['target'] = None
-    data['shift'] = None
 
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as p:
-        p.map_async(features_extraction, all_samples, callback=end_extraction)
+        p.map_async(features_extraction, all_samples)
         p.close()
         p.join()
-    #sample_channels = read_sig(f"{dataset_path}/{all_samples[2]}")
-    #print(select_wind(all_samples[2], len(sample_channels[CHANELS[0]])))
--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,6 @@
 from catboost import CatBoostClassifier, CatBoostRegressor, Pool
 import numpy as np
 from clf_data_transform import read_sig, metrics
-from reg_data_transform import decimate_signal
 
 FREQUENCY = 256
 WINDOW_SIZE = 30
@@ -10,19 +9,17 @@ CHANELS = ['FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-
 
 
 class SeizureModel:
-    def __init__(self, clf_model, reg_model):
+    def __init__(self, clf_model):
         self.clf_model = CatBoostClassifier().load_model(clf_model)
-        self.reg_model = CatBoostRegressor().load_model(reg_model)
         self.overlap = 0
         self.sig = None
         self.len_sig = None
-        self.K = 16
 
     def set_overlap(self, overlap):
-        if 30 > overlap > 0:
+        if WINDOW_SIZE > overlap > 0:
             self.overlap = overlap
         else:
-            print("Overlap must be between 0 and 30")
+            print(f"Overlap must be between 0 and {WINDOW_SIZE}")
 
     def __select_windows(self):
         extracted_windows = []
@@ -31,9 +28,7 @@ class SeizureModel:
         windows_start = np.append(windows_start, self.len_sig//FREQUENCY-WINDOW_SIZE)
         for start_window_time in windows_start:
             indices = np.arange(start_window_time*FREQUENCY, (start_window_time + WINDOW_SIZE)*FREQUENCY, dtype=int)
-            extract = np.array([])
-            for ch in CHANELS:
-                extract = np.append(extract, metrics(self.sig[ch][indices]), axis=0)
+            extract = metrics(self.sig[:, indices])
             extracted_windows.append(extract)
             start_time.append(start_window_time*FREQUENCY)
         return extracted_windows, start_time
@@ -47,37 +42,21 @@ class SeizureModel:
 
         return detected
 
-    def __detect_start_time(self, windows_start):
-
-        detected_start_time = []
-        for w in windows_start:
-            data = np.array([])
-            indices = np.arange(w, w + WINDOW_SIZE * FREQUENCY, dtype=int)
-            for ch in CHANELS:
-                data = np.append(data, decimate_signal(self.sig[ch][indices], self.K), axis=0)
-            for ch in CHANELS:
-                data = np.append(data, metrics(self.sig[ch][indices]), axis=0)    
-            
-            detected_start_time.append(w//FREQUENCY + int(self.reg_model.predict(data)))
-
-        return detected_start_time
 
     def predict(self, sample):
         self.sig, self.len_sig = read_sig(sample)
         detected_windows = self.__detect_windows()
         if detected_windows:
-            detected_start_time = self.__detect_start_time(detected_windows)
-            for s in detected_start_time:
+            for s in detected_windows:
                 print(f"Обнаружен всплеск на {s} секунде")
-            return detected_start_time
+            return detected_windows
         else:
             print(f"Всплесков не обнаружено")
             return []
 
 
 if __name__ == '__main__':
-    s = "/home/didges/ds_learning/EEG/physionet.org/files/chbmit/1.0.0/chb04/chb04_28.edf"
-    model = SeizureModel('window_detection.cbm', 'start_time.cbm')
-    #model.set_overlap(15)
-    model.predict(s)
+    dataset_path = "/path/to/edf"
+    model = SeizureModel('window_detection.cbm')
+    model.predict(dataset_path)
 
